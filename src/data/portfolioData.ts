edd060d21ec7efde8f427969495b3c69687bac13
Viona Kaleb <vionakaleb@gmail.com>
2026-06-12 09:48:00 +0700

    Upload

--- src/data/portfolioData.ts
+++ src/data/portfolioData.ts
@@ -7,7 +7,7 @@ export const portfolioData: PortfolioData = {
     description: "A Software Engineer",
     image: "profilepic.jpg",
     bg: "profile-bg.JPG",
-    resume: "Software-Engineer-Frontend-Viona-Kaleb.pdf",
+    resume: "Frontend Developer Resume - Viona Kaleb.pdf",
     bios: [""],
     bios2: [
       "- Developing features from UI/UX designs",
